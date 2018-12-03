55704a0454aee8b1fef6b5d14ff7ae26c2f33086
ildafon <ildafon@yandex.ru>
2018-12-03 07:31:54 +0300

    material starter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+import {MaterialModule} from './material.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
